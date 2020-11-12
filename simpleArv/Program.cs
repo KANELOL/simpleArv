2728fcc460e896f02ee3dd3e25784ee43b3a75ba
KANELOL <65165205+KANELOL@users.noreply.github.com>
2020-11-12 08:06:15 +0100

    Add files via upload

---
+++ simpleArv/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace simpleArv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var screamer = new ClassThatInheritances(7,3,"Mee awesome", "Me very very awesome");
+           var god = new TheOneThatInheritsITAllAndSetsIt();
+            Console.WriteLine(screamer.screamMyStats() +" StatScreamed1\n");
+            Console.WriteLine(screamer.screamMyStats2() +" StatScreamed2\n");
+            Console.WriteLine("The one willl now speak:\n");
+            Console.WriteLine(god.screamMyStats2());
+        }
+    }
+}
---
+++ simpleArv/TheOneThatInheritsITAllAndSetsIt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace simpleArv
+{
+    class TheOneThatInheritsITAllAndSetsIt : ClassThatInheritances
+    {
+        public TheOneThatInheritsITAllAndSetsIt() : base(20, 100, "I AM ", "THE INHERITANCE GOD!")
+        {
+        }
+    }
+}
